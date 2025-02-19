e12f41b9a2e57e87d1c00e123c51bcc203f8db0f
brenohsilva <brennohsilva@hotmail.com>
2025-02-19 12:20:33 -0300

    :recycle: refactor: add hashed password to users

--- src/users/usecases/create-user.usecase.ts
+++ src/users/usecases/create-user.usecase.ts
@@ -1,13 +1,25 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from '../users.service';
 import { CreateUserDto } from '../dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class CreateUserUseCase {
   constructor(private readonly usersService: UsersService) {}
+
   async execute(data: CreateUserDto) {
     try {
-      const response = await this.usersService.create(data);
+      const hashedPassword = await bcrypt.hash(data.password, 10);
+
+      const userDataWithHashedPassword = {
+        ...data,
+        password: hashedPassword,
+      };
+
+      const response = await this.usersService.create(
+        userDataWithHashedPassword,
+      );
+
       if (response) {
         return {
           success: true,
