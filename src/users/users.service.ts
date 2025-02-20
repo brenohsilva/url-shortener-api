379c210efda887a3c2f1a57bc7b5d005609353f9
brenohsilva <brennohsilva@hotmail.com>
2025-02-19 23:04:21 -0300

    :broom: cleanup: remove eslint comments from code

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
