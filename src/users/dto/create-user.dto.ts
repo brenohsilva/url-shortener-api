ae446bb9713660a026cae2899ac0bb0de3844568
brenohsilva <brennohsilva@hotmail.com>
2025-02-20 16:59:49 -0300

    :label: add swagger type in user dtos

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ example: 'John Doe', description: 'Nome do usuário' })
   name: string;
+
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    description: 'E-mail do usuário',
+  })
   email: string;
+
+  @ApiProperty({
+    example: '123456',
+    description: 'Senha do usuário',
+    minLength: 6,
+  })
   password: string;
 }
