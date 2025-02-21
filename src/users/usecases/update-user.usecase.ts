1119da45f166f64b5fafa3e8c64bf2b949ec2167
brenohsilva <brennohsilva@hotmail.com>
2025-02-21 11:51:50 -0300

    :bug: fix: crypt user password when to be updated

--- src/users/usecases/update-user.usecase.ts
+++ src/users/usecases/update-user.usecase.ts
@@ -2,22 +2,33 @@
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { UsersService } from '../users.service';
 import { UpdateUserDto } from '../dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UpdateUserUseCase {
   constructor(private readonly usersService: UsersService) {}
   private readonly logger = new Logger(UpdateUserUseCase.name);
+
   async execute(id: string, data: UpdateUserDto) {
     try {
-      const response = await this.usersService.findOne(id);
-      if (!response) {
+      const user = await this.usersService.findOneById(id);
+      if (!user) {
         throw new HttpException(
           'Erro ao trazer o usuário. Usuário não encontrado.',
           HttpStatus.NOT_FOUND,
         );
       }
 
-      const updateResponse = await this.usersService.update(id, data);
+      const updatedData = { ...data };
+
+      if (data.password) {
+        const hashedPassword = await bcrypt.hash(data.password, 10);
+        updatedData.password = hashedPassword;
+      } else {
+        delete updatedData.password;
+      }
+
+      const updateResponse = await this.usersService.update(id, updatedData);
 
       return {
         success: true,
@@ -26,7 +37,7 @@ export class UpdateUserUseCase {
     } catch (error) {
       this.logger.error(error);
       throw new HttpException(
-        'Erro ao atualzar o usuário. Tente novamente mais tarde.',
+        'Erro ao atualizar o usuário. Tente novamente mais tarde.',
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
