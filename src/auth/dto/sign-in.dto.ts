9dc7ea1a16b777bcb3397e90fedea128968eabc8
brenohsilva <brennohsilva@hotmail.com>
2025-02-21 12:53:17 -0300

    :recycle: refactor: add example in swagger property of signin

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,13 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class SignInDto {
-  @ApiProperty()
+  @ApiProperty({ example: 'joe.doe@hotmail.com', description: 'e-mail' })
   @IsEmail({}, { message: 'O e-mail deve ser válido' })
   @IsNotEmpty({ message: 'O e-mail é obrigatório' })
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: '123456', description: 'senha' })
   @IsNotEmpty({ message: 'A senha é obrigatório' })
   password: string;
 }
