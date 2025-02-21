88b7cab15324b4548b863404cc855ae7e5ac756b
brenohsilva <brennohsilva@hotmail.com>
2025-02-21 12:25:42 -0300

    :sparkles: feat: add shortened decorators to swagger documentation

--- src/shortened_urls/dto/create-shortened_url.dto.ts
+++ src/shortened_urls/dto/create-shortened_url.dto.ts
@@ -1,10 +1,38 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateShortenedUrlDto {
+  @ApiProperty({
+    example: 'cbcc38ca-b3d5-411a-b355-511514c26edb',
+    description: 'Id do usuário',
+  })
+  @IsString()
   users_id?: string;
+  @ApiProperty({
+    example: 'KJumIA',
+    description: 'Codigo de encurtamento',
+  })
+  @IsString()
   short_code: string;
+  @ApiProperty({
+    example: 'https://teddydigital.io',
+    description: 'Url para ser encurtada',
+  })
+  @IsString()
   original_url: string;
+  @ApiProperty({
+    example: 'http://localhost:3000/KJumIA',
+    description: 'Url encurtada',
+  })
   shorten_url: string;
 }
 
 export class ShortenedUrlBodyDto {
-  url: string
+  @ApiProperty({
+    example: 'https://teddydigital.io',
+    description: 'Url para ser encurtada',
+  })
+  @IsString()
+  @IsNotEmpty()
+  url: string;
 }
