814bece59b32aa0a8231f4600490bab18a62e9e6
brenohsilva <brennohsilva@hotmail.com>
2025-04-20 11:39:42 -0300

    :sparkles: feat: add shortcode verification

--- src/urls/shortener_urls/usecases/create-shortener-url.usecase.ts
+++ src/urls/shortener_urls/usecases/create-shortener-url.usecase.ts
@@ -11,6 +11,7 @@ import { ShortenerUrlsService } from '../shortener-urls.service';
 import { ShortenerUrlBodyDto } from '../dto/create-shortener_url-body.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { NotFoundError } from 'src/common/errors/not-found.error';
+import { AlreadyExistsError } from 'src/common/errors/already-exists.error';
 
 @Injectable()
 export class CreateShortenerUrlUseCase {
@@ -23,53 +24,54 @@ export class CreateShortenerUrlUseCase {
   private readonly logger = new Logger(CreateShortenerUrlUseCase.name);
 
   async execute(data: ShortenerUrlBodyDto, request: Request) {
-    try {
-      const { origin_url, short_code, comments, tags } = data;
-      const shortCode = short_code || generateUniqueCode();
-      const baseUrl = process.env.BASE_URL || 'http://localhost:4000';
-      const shortenUrl = `${baseUrl}/${shortCode}`;
+    const { origin_url, short_code, comments, tags } = data;
+    let shortCode = short_code || generateUniqueCode();
+    const isShortCodeExists =
+      await this.shortenedUrlService.findByShortCode(shortCode);
 
-      const accessToken = JwtToken(request);
-      const user: UserDto = await this.jwtService.decode(accessToken.trim());
-      const userId = user?.sub ?? null;
+    if (isShortCodeExists && short_code) {
+      throw new AlreadyExistsError('url', 'short_code', short_code);
+    } else if (isShortCodeExists) {
+      shortCode = generateUniqueCode();
+    }
 
-      let workspacesId: string | undefined;
+    const baseUrl = process.env.BASE_URL || 'http://localhost:4000';
+    const shortenUrl = `${baseUrl}/${shortCode}`;
 
-      if (userId) {
-        const workspace = await this.prismaService.workspaces.findFirst({
-          where: {
-            slug: 'bcprodutos',
-            deleted_at: null,
-          },
-        });
+    const accessToken = JwtToken(request);
+    const user: UserDto = await this.jwtService.decode(accessToken.trim());
+    const userId = user?.sub ?? null;
 
-        if (!workspace) {
-          throw new NotFoundError('workspace', 'id', 'bcprodutos');
-        }
+    let workspacesId: string | undefined;
 
-        workspacesId = workspace.id;
+    if (userId) {
+      const workspace = await this.prismaService.workspaces.findFirst({
+        where: {
+          slug: 'bcprodutos',
+          deleted_at: null,
+        },
+      });
+
+      if (!workspace) {
+        throw new NotFoundError('workspace', 'id', 'bcprodutos');
       }
 
-      const newData: CreateShortenerUrlDto = {
-        origin_url,
-        short_code: shortCode,
-        shorten_url: shortenUrl,
-        ...(comments && { comments }),
-        ...(tags && { tags }),
-        ...(userId && { users_id: Number(userId) }),
-        ...(workspacesId && { workspaces_id: workspacesId }),
-        ...(!userId && { expires_at: new Date(Date.now() + 30 * 60 * 1000) }),
-      };
-
-      await this.shortenedUrlService.create(newData);
-
-      return { shortenUrl: shortenUrl };
-    } catch (error) {
-      this.logger.error(error);
-      throw new HttpException(
-        'Erro ao gerar a url. Tente novamente mais tarde.',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      workspacesId = workspace.id;
     }
+
+    const newData: CreateShortenerUrlDto = {
+      origin_url,
+      short_code: shortCode,
+      shorten_url: shortenUrl,
+      ...(comments && { comments }),
+      ...(tags && { tags }),
+      ...(userId && { users_id: Number(userId) }),
+      ...(workspacesId && { workspaces_id: workspacesId }),
+      ...(!userId && { expires_at: new Date(Date.now() + 30 * 60 * 1000) }),
+    };
+
+    await this.shortenedUrlService.create(newData);
+
+    return { shortenUrl: shortenUrl };
   }
 }
