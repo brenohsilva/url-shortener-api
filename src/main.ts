2e92d7571704cbfa64f52d5967bcd167a729d039
brenohsilva <brennohsilva@hotmail.com>
2025-04-30 22:33:02 -0300

    :sparkles: add observability

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
+/* eslint-disable @typescript-eslint/await-thenable */
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { UserAlreadyExistsErrorFilter } from './identity/users/filters/user-already-exists.filter';
 import { UserNotFoundErrorFilter } from './identity/users/filters/user-not-found.filter';
+import { otelSDK } from './tracing';
 
 async function bootstrap() {
+  
+  await otelSDK.start();
+
   const app = await NestFactory.create(AppModule);
-  
-  app.enableCors()
-  
+
+  app.enableCors();
+
   app.useGlobalFilters(
     new UserAlreadyExistsErrorFilter(),
     new UserNotFoundErrorFilter(),
---
+++ src/tracing.ts
@@ -0,0 +1,41 @@
+import {
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-base';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import * as process from 'process';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
+import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
+
+const jaegerExporter = new JaegerExporter({
+  endpoint: 'http://localhost:14268/api/traces',
+});
+
+const traceExporter = jaegerExporter;
+
+export const otelSDK = new NodeSDK({
+  resource: resourceFromAttributes({
+    [SEMRESATTRS_SERVICE_NAME]: 'api-service',
+  }),
+  spanProcessor: new SimpleSpanProcessor(traceExporter),
+  instrumentations: [
+    new HttpInstrumentation(),
+    new ExpressInstrumentation(),
+    new NestInstrumentation(),
+  ],
+});
+
+// gracefully shut down the SDK on process exit
+process.on('SIGTERM', () => {
+  otelSDK
+    .shutdown()
+    .then(
+      () => console.log('SDK shut down successfully'),
+      (err) => console.log('Error shutting down SDK', err),
+    )
+    .finally(() => process.exit(0));
+});
